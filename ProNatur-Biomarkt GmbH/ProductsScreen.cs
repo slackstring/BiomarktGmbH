d417e11eff5cfebe489cbbc8473493eece9e0c45
slackstring <slackstring88@gmail.com>
2023-11-19 14:46:08 +0100

    Added DB / Connection to DB

---
+++ ProNatur-Biomarkt GmbH/ProductsScreen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProNatur_Biomarkt_GmbH
+{
+    public partial class ProductsScreen : Form
+    {
+        private SqlConnection databaseConnection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\Andre\Documents\Pro-Natur Biomarkt GmbH.mdf;Integrated Security = True; Connect Timeout = 30");
+        public ProductsScreen()
+        {
+            InitializeComponent();
+
+            //Start
+            databaseConnection.Open();
+
+            string query = "select * from Products";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, databaseConnection);
+            DataSet dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet);
+
+            productsDGV.DataSource = dataSet.Tables[0];
+            productsDGV.Columns[0].Visible = false;
+
+
+
+
+            databaseConnection.Close();
+
+        }
+
+        private void btnProductSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProductEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProductClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProductDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
